a27167e9d816b540e3a89d34fb7a22d7e8f73511
Daniel Davies <daniel.davies@aviva.co.uk>
2012-10-18 09:57:53 +0100

    admin added

--- shorty/admin.py
+++ shorty/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from short.models import ShortURl
+from shorty.models import ShortUrl
 
 
-admin.site.register(ShortUrl)
+class ShortUrlAdmin(admin.ModelAdmin):
+    list_display = ('url', 'clicks', 'short_code')
+
+admin.site.register(ShortUrl, ShortUrlAdmin)
