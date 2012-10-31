6d1ffe33b2d3c127a6cf7d1c28f1f72448445d07
Daniel Davies <daniel.davies@aviva.co.uk>
2012-10-31 19:34:41 +0000

    Safe mode

--- shorty/views.py
+++ shorty/views.py
@@ -1,11 +1,18 @@
 from django.conf import settings
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
+from django.shortcuts import render_to_response
 
 from shorty.models import ShortUrl, Click
 
 
 def convert(request, code):
-    short_url = ShortUrl.objects.get_by_code(code)
+    try:
+        short_url = ShortUrl.objects.get_by_code(code)
+    except ShortUrl.:
+        Http404()
+        
+    safe_mode = request.GET.get('safe_mode', False)
+
     short_url.clicks += 1
     short_url.save()
 
@@ -16,6 +23,11 @@ def convert(request, code):
         click.url = short_url.url
         click.unique_id = short_url.unique_id
         click.pk = short_url.pk
+        if safe_mode:
+            click.safe_mode = True
         click.save()
 
-    return HttpResponseRedirect(short_url.url)
+    if safe_mode:
+        return render_to_response("shorty/safe-mode.html", {'short_url': short_url})
+    else:
+        return HttpResponseRedirect(short_url.url)
